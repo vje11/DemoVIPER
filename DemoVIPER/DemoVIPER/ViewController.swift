2598e16e166436034c03721d71997b38139c657b
Vijay <vijay@testbook.com>
2024-07-16 23:01:54 +0530

    Base setup with local packages
    - Core, UIComponents and Networking packages added
    - Splash screen label added with Demo Viper text on it

---
+++ DemoVIPER/DemoVIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoVIPER
+//
+//  Created by Vijay  on 16/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
